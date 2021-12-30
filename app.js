994907b5fa1c9dc26e282ce06fdf7de16844444d
KyPT <kypt@rikkeisoft.com>
2021-12-30 15:36:28 +0700

    chat

---
+++ app.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var express = require("express");
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+//Chỉ ra đường dẫn chứa css, js, images...
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Tạo router
+app.get("/", function (req, res) {
+    console.log(req.cookies);
+    res.sendFile(path.join(__dirname + '/views/chat.html'));
+});
+app.get("/123",function (req,res){
+    console.log(req.cookies);
+})
+
+//Tạo socket 
+io.on('connection', function (socket) {
+    console.log('Welcome to server chat');
+
+    socket.on('send', function (data) {
+        io.sockets.emit('send', data);
+    });
+});
+
+//Khởi tạo 1 server listen tại 1 port
+server.listen(8080);
+
